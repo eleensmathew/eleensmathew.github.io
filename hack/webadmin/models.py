cff1b424b85d6329e06109a4546bb5c128d57a1d
eleensmathew <eleensmathew@gmail.com>
2023-03-25 03:01:49 +0530

    small changes

--- hack/webadmin/models.py
+++ hack/webadmin/models.py
@@ -1,16 +1,22 @@
 from django.db import models
-import jsonfield
+
 # Create your models here.
 
-class Priority(models.Model):
-    pq = jsonfield.JSONField(default=[])
-    def __str__(self):
-        return str(self.pq)
+
     
 class Admin_info(models.Model):
     username = models.CharField(max_length=100)
     password = models.CharField(max_length=50)
     email = models.EmailField(verbose_name="Email", null=True, unique=True, max_length=100)
-    priorityq = jsonfield.JSONField(default={})
+    #priority = models.ForeignKey(Priority,on_delete=models.CASCADE)
+    
     def __str__(self):
         return self.username
+    
+class Priority(models.Model):
+    pq=models.IntegerField() #holds the priority of the video in the database
+    pk_value=models.IntegerField(default=0) #used for getting the referrence no of the video
+    admin_associated = models.ForeignKey(Admin_info,on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return str(self.pq)
--- hack/webadmin/views.py
+++ hack/webadmin/views.py
@@ -1,24 +1,22 @@
 from django.shortcuts import render
 from .models import Priority, Admin_info
-import heapq
 # Create your views here.
 
-def select_admin(): #Selecting which admin should monitor that video recording
+#def select_admin(): #Selecting which admin should monitor that video recording
     
-def get_video(request):    #Which videos the admins have to currently watch respectively
-    vid = Priority.objects.filter(pq=1, admin_associated=request.user)
+# def get_video():    #Which videos the admins have to currently watch respectively
+    
+# def assign_priority(assigned):  #setting the priority of the new video in selected admin's queue
     
     
-def assign_priority():  #given based on criteria
-    
-def next_video():   #video the admin has to watch after they finished watching the previous and delete previos from queue
+# def next_video():   #video the admin has to watch after they finished watching the previous and delete previos from queue
   
-def add_video_details():    #add video in admin's queue
-    admin_assigned = select_admin() #object
-    # admin_json_field_insert_(pq, pk)
-    getq = admin_assigned.priorityq
-    pq = assign_priority()
-    getq.append({})
-    heapq.heapify(getq)     # heapify
-    admin_assigned.save()
-    # proirity++
+#def add_video_details():    #add video in admin's queue
+      #create the object having pq and pk of new video
+      #pk will be fetched from the cloud storage
+      #pq will be calculated based on hash function for assigning priority to the events
+    #   admin_assigned = select_admin() #object
+    #   priority_val = assign_priority(admin_assigned)
+    #   #pk_val = fetched
+    #   new_video = Priority.objects.create(pq=priority_val, pk_value=pk_val, admin_associated=admin_assigned)
+    #   new_video.save()
