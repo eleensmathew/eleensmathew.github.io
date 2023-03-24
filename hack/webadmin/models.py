a477cb07dd5d72834694551c75d417ed0e5d0388
Nandika-A <nandikaagrawal610@gmail.com>
2023-03-25 02:16:09 +0530

    defined priority queue assigning functions

--- hack/webadmin/models.py
+++ hack/webadmin/models.py
@@ -1,16 +1,11 @@
 from django.db import models
-import jsonfield
 # Create your models here.
-
-class Priority(models.Model):
-    pq = jsonfield.JSONField(default=[])
-    def __str__(self):
-        return str(self.pq)
     
 class Admin_info(models.Model):
     username = models.CharField(max_length=100)
     password = models.CharField(max_length=50)
     email = models.EmailField(verbose_name="Email", null=True, unique=True, max_length=100)
-    priorityq = jsonfield.JSONField(default={})
+    priorityq = models.JSONField(default={})
+    size = models.IntegerField(default=0)
     def __str__(self):
         return self.username
--- hack/webadmin/views.py
+++ hack/webadmin/views.py
@@ -1,24 +1,34 @@
 from django.shortcuts import render
 from .models import Priority, Admin_info
 import heapq
+import copy
+from django.db.models import Min
 # Create your views here.
 
 def select_admin(): #Selecting which admin should monitor that video recording
+    adminsel = Admin_info.objects.all().order_by(-'size').first()
+    return adminsel
     
 def get_video(request):    #Which videos the admins have to currently watch respectively
-    vid = Priority.objects.filter(pq=1, admin_associated=request.user)
-    
+    admin = request.user.username
+    admin_obj = Admin_info.objects.get(username = admin)
+    vid_key = next(iter(admin_obj.priorityq))
+    video = admin_obj.priorityq.get(vid_key)
+    return render(request, "webadmin/Showrec.html", {"video" : video})
     
 def assign_priority():  #given based on criteria
     
 def next_video():   #video the admin has to watch after they finished watching the previous and delete previos from queue
   
-def add_video_details():    #add video in admin's queue
+def add_video_details(request, v_id):    #add video in admin's queue
     admin_assigned = select_admin() #object
     # admin_json_field_insert_(pq, pk)
-    getq = admin_assigned.priorityq
-    pq = assign_priority()
-    getq.append({})
-    heapq.heapify(getq)     # heapify
+    getq = dict(admin_assigned.priorityq)
+    p = assign_priority()
+    getq[p] = v_id #video id
+    di = list(getq.items()) # heapify
+    heapq.heapify(di)     # heapify
+    di = dict(di)   # heapify
+    admin_assigned.size += 1
+    admin_assigned.priorityq = di.copy()
     admin_assigned.save()
-    # proirity++
